0892f90d852d232decd145c6fdbdd4d7ba240252
Firmin Launay <hey@firminlaunay.me>
2021-07-15 18:45:45 +0200

    Everything is functioning smoothly

--- divia_req.py
+++ divia_req.py
@@ -1,5 +1,17 @@
 from divia_api import DiviaAPI
+from datetime import date, datetime
+from browser import window
 
-#api = DiviaAPI()
+api = DiviaAPI()
 
-print(DiviaAPI().find_line("T1", "A").find_stop("Godrans").totem())
+
+def bridge(ligne, sens, arret):
+    totem_result = api.find_stop(ligne, arret, sens).totem()
+    now = datetime.now()
+    processed_result = []
+    for t in totem_result:
+        processed_result.append(str(round((t - now).total_seconds() / 60)))
+    return processed_result
+
+
+window.diviaApiBridge = bridge
--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+document.body.addEventListener("keyup", (e) => {
+    if (e.keyCode == 13) {
+        e.preventDefault()
+        searchDivia();
+    }
+    return false;
+});
+
+
 function elementFocus(el) {
     if (el.value == el.dataset.defaultValue) el.value = "";
     el.style.color = "black";
@@ -7,5 +16,42 @@ function elementFocus(el) {
             el.style.color = "grey";
             el.value = el.dataset.defaultValue;
         }
-    })
+    });
+}
+
+
+function searchDivia() {
+    document.getElementById("result_wrapper").style.display = "none";
+    document.getElementById("spinner_wrapper").style.display = "block";
+    const ligne = document.getElementById("ligne_input").value;
+    const sens = document.getElementById("sens_dropdown").value;
+    const arret = document.getElementById("arret_input").value;
+    if (ligne == "" || sens == "none" || arret == "") return alert("❌ Merci de remplir tous les champs !")
+    try {
+        const result = diviaApiBridge(ligne, sens, arret);
+        let res1 = "—";
+        let res2 = "—"
+        if (result.length > 0) {
+            if (result[0] || result[0] == 0) {
+                if (result[0] < 1) {
+                    res1 = "À l’approche";
+                } else {
+                    res1 = result[0].toString() + " min";
+                }
+                if (result[1] || result[1] == 0) {
+                    if (result[1] < 1) {
+                        res2 = "À l’approche";
+                    } else {
+                        res2 = result[1].toString() + " min";
+                    }
+                }
+                document.getElementById("res1").innerText = res1;
+                document.getElementById("res2").innerText = res2;
+                document.getElementById("result_wrapper").style.display = "block";
+                return document.getElementById("spinner_wrapper").style.display = "none";
+            }
+        }
+    } catch {}
+    alert("❌ Nous n’avons rien trouvé :(");
+    document.getElementById("spinner_wrapper").style.display = "none";
 }
